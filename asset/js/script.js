bf48deceb589eb0e31d092fc04ffeb0f3a2ebde0
Asela <43272744+zaselalk@users.noreply.github.com>
2023-09-30 22:12:53 +0530

    :tada: Basic version implemented

---
+++ asset/js/script.js
@@ -0,0 +1,6 @@
+const resourceSection = document.getElementById("resource");
+
+setTimeout(() => {
+  resourceSection.classList.remove("hidden");
+  //   resourceSection.style.display = "flex";
+}, 10000);
